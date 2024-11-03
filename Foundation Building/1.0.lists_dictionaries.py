2c7e769fbe418229b1e876780671cb829462f310
Manases Lovera <manaseslovera07@gmail.com>
2024-11-03 14:50:36 -0400

    dictionaries: working with dictionaries

--- Foundation Building/1.0.lists_dictionaries.py
+++ Foundation Building/1.0.lists_dictionaries.py
@@ -2,3 +2,8 @@
 numbers = [10, 20, 40, 40, 50]
 average = sum(numbers) / len(numbers)
 print("Average: ",average)
+
+# Working with dictionaries
+data = {"apple": 3, "banana": 5, "orange": 2}
+for fruit, quantity in data.items():
+    print(f"{fruit}: {quantity}")
