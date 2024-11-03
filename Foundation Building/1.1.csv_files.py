7cac577079a435dc3a119fe80ebef705152bbdcf
Manases Lovera <manaseslovera07@gmail.com>
2024-11-03 13:09:07 -0400

    foundation: new excercise added to csv_files

--- Foundation Building/1.1.csv_files.py
+++ Foundation Building/1.1.csv_files.py
@@ -16,3 +16,18 @@ data = [
 with open("1.1.output.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(data)
+
+# Students
+total_grade = 0
+count = 0
+
+with open('1.1.students.csv','r') as file:
+    reader = csv.reader(file)
+    next(reader) # Skip header row
+    for row in reader:
+        name, grade = row
+        total_grade += int(grade)
+        count += 1
+
+average_grade = total_grade / count
+print('Average grade:', average_grade)
